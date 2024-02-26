1dce7b157eb71700ca074d8052a39c451d7c4092
taha cherif <58517938+tahacherif@users.noreply.github.com>
2024-02-27 00:00:59 +0100

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,12 +1,9 @@
-
 @extends('master')
 @section("content")
 <div class="container custom-login">
   <div class="row">
     <div class="col-sm-4 col-sm-offset-4">
-    <button (click)="signin()">
-    SIGN IN WITH GOOGLE
-    </button>
+
     <form action="login" method="POST">
   <div class="form-group">
       @csrf
@@ -19,58 +16,47 @@
   </div>
   <button style = "font-family:Calibri;font-size:25px;" type="submit" class="btn btn-default">Login</button>
     </form>
-    <button (click)="signin()">
-    SIGN IN WITH GOOGLE
-    </button>
+    <div class="mb-0 text-center">
+
+                  <a class="btn mt-3 shadow-lg bg-white rounded"  onClick="socialSignin('google');">
+                    <img width="20px" style="margin-bottom:3px; margin-right:5px" alt="Google sign-in" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png" />
+                    <span class="fa fa-google"></span> Sign in with Google
+                  </a>
     </div>
   </div>
+
 </div>
+
+<script src="https://www.gstatic.com/firebasejs/7.14.0/firebase-app.js"></script>
+      <script src="https://www.gstatic.com/firebasejs/7.14.0/firebase-auth.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+      <script>
+          var config = {
+            apiKey: "AIzaSyCb8E-OY-HyMSL4cW6bltRr4F7KdrPcc4M",
+  authDomain: "patisseriedaoud.firebaseapp.com",
+  projectId: "patisseriedaoud",
+  storageBucket: "patisseriedaoud.appspot.com",
+  messagingSenderId: "412574467583",
+  appId: "1:412574467583:web:3d151783594019a90393ca",
+  measurementId: "G-XEYHSH6C9S"         
+      };
+      firebase.initializeApp(config);
+    
+      var googleProvider = new firebase.auth.GoogleAuthProvider();
+      var googleCallbackLink = '/login/google/callback';
+      async function socialSignin(provider) {
+                     document.getElementById('social-login-form').action = googleCallbackLink;
+         }
+      firebase.auth().signInWithPopup(googleProvider).then(function(result) {
+            result.user.getIdToken().then(function(result) {
+               document.getElementById('social-login-tokenId').value = result;
+               document.getElementById('social-login-form').submit();
+            });
+         }).catch(function(error) {
+            // do error handling
+            console.log(error);
+         });
+      
+      </script>
+
       @endsection
-<script src="https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js"></script>
-<!-- Add other Firebase services if needed -->
-<script>
-
-<script>
-    function signin() {
-        firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider())
-            .then(function(result) {
-                // Handle successful sign-in
-                window.location.href = '/members';
-            })
-            .catch(function(error) {
-                // Handle errors
-                console.error("Sign-in error:", error);
-            });
-    }
-
-    function logout() {
-        firebase.auth().signOut()
-            .then(function() {
-                // Sign-out successful.
-                window.location.href = '/login';
-            })
-            .catch(function(error) {
-                // An error happened.
-                console.error("Logout error:", error);
-            });
-    }
-</script>
-
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyCg0JB55Sz9AJ7fGaFS-iOHNo6EiyulkMo",
-  authDomain: "laravel-c1754.firebaseapp.com",
-  projectId: "laravel-c1754",
-  storageBucket: "laravel-c1754.appspot.com",
-  messagingSenderId: "842358056004",
-  appId: "1:842358056004:web:31d57c6b7924630e34c811",
-  measurementId: "G-ZWFRB9HPB3"
-};
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-</script>
